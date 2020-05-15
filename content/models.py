94486074e50b8d222e257e191f8d8aaccc6678d3
shooming <liomily@gmail.com>
2020-05-15 19:29:44 +0900

    user, content 앱 생성 및 models.py 작성 수정 사항 수정

---
+++ content/models.py
@@ -0,0 +1,151 @@
+from django.db         import models
+
+class RootCategory(models.Model):
+    name  = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'root_categories'
+
+class MiddleCategory(models.Model):
+    root_category = models.ForeignKey('RootCategory', on_delete=models.SET_NULL, null=True)
+    name          = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'middle_categories'
+
+class EndCategory(models.Model):
+    name = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'end_categories'
+
+class MiddleContentTag(models.Model):
+    middle_category  = models.ForeignKey('MiddleCategory', on_delete=models.SET_NULL, null=True)
+    Content          = models.ForeignKey('Content', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'middle_content_tags'
+
+class EndContentTag(models.Model):
+    end_categorry  = models.ForeignKey('EndCategory', on_delete=models.SET_NULL, null=True)
+    content        = models.ForeignKey('content', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'end_content_tags'
+
+class Content(models.Model):
+    teacher          = models.ForeignKey('user.Teacher', on_delete=models.SET_NULL, null=True)
+    meditaion_type   = models.ForeignKey('ContentType', on_delete=models.SET_NULL, null=True)
+    activity_type    = models.ForeignKey('ActivityType', on_delete=models.SET_NULL, null=True)
+    target           = models.ForeignKey('Target', on_delete=models.SET_NULL, null=True)
+    title            = models.CharField(max_length = 100)
+    description      = models.CharField(max_length = 2000)
+    image_url        = models.URLField(max_length = 2000)
+    file_source      = models.CharField(max_length = 2000)
+    running_time     = models.TimeField()
+
+    class Meta:
+        db_table = 'contents'
+
+class ContentType(models.Model):
+    name   = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'content_types'
+
+class ActivityType(models.Model):
+    name   = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'activity_types'
+
+class Target(models.Model):
+    name = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'targets'
+
+class ContentReview(models.Model):
+    content         = models.ForeignKey('Content', on_delete=models.SET_NULL, null=True)
+    user            = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
+    content_rating  = models.ForeignKey('ContentRating', on_delete=models.SET_NULL, null=True)
+    review          = models.CharField(max_length = 2000)
+    write_date      = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        db_table = 'content_reviews'
+
+class ContentRating(models.Model):
+    rating = models.IntegerField(default = 0)
+
+    class Meta:
+        db_table = 'content_ratings'
+
+class Course(models.Model):
+    payment_status  = models.ForeignKey('user.PaymentStatus', on_delete=models.SET_NULL, null=True)
+    teacher         = models.ForeignKey('user.Teacher', on_delete=models.SET_NULL, null=True)
+    course_name     = models.CharField(max_length =50)
+    course_info     = models.CharField(max_length = 2000)
+
+    class Meta:
+        db_table = 'courses'
+
+class CourseContent(models.Model):
+    course           = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
+    lesson_title     = models.CharField(max_length = 100)
+    lesson_describe  = models.CharField(max_length = 2000)
+
+    class Meta:
+        db_table = 'course_contents'
+
+class CourseReview(models.Model):
+    user          = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
+    course        = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
+    course_rating = models.ForeignKey('CourseRating', on_delete=models.SET_NULL, null=True)
+    review        = models.CharField(max_length = 2000)
+    write_date    = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        db_table = 'course_reviews'
+
+class CourseRating(models.Model):
+    rating  = models.IntegerField(default = 0)
+
+    class Meta:
+        db_table = 'course_ratings'
+
+class PlayList(models.Model):
+    teacher   = models.ForeignKey('user.Teacher', on_delete=models.SET_NULL, null=True)
+    title     = models.CharField(max_length = 100)
+    describe  = models.CharField(max_length = 2000)
+    pick      = models.BooleanField()
+
+    class Meta:
+        db_table = 'play_lists'
+
+class TagGroup(models.Model):
+    play_list = models.ForeignKey('PlayList', on_delete=models.SET_NULL, null=True)
+    tag_list  = models.ForeignKey('TagList', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'tag_groups'
+
+class TagList(models.Model):
+    tag  = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'tag_lists'
+
+class PlayListGroup(models.Model):
+    play_list  = models.ForeignKey('PlayList', on_delete=models.SET_NULL, null=True)
+    content    = models.ForeignKey('Content', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'play_list_groups'
+
+class PlayListFollower(models.Model):
+    user      = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
+    play_list = models.ForeignKey('PlayList', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = 'play_list_followers'
--- wesight/settings.py
+++ wesight/settings.py
@@ -32,21 +32,19 @@ ALLOWED_HOSTS = my_settings.ALLOWED_HOSTS
 # Application definition
 
 INSTALLED_APPS = [
-#   'django.contrib.admin',
-#   'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'user',
+    'content',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-#   'django.middleware.csrf.CsrfViewMiddleware',
-#   'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
